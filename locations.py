a87b388436078314630792b860ca941a93fa5019
martijnbentum <bentummartijn@gmail.com>
2026-03-06 10:39:17 +0100

    handle paths

---
+++ locations.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+data = Path(__file__).parent.parent
+
+
+excel_filename = '../../trialsNewTryWithReps.ods'
+
+audio_info_dict = Path('../audio_info.json')
+
+stimuli = data / 'tom-eye'
+
+audio_fn= list(stimuli.glob('*.wav'))
+textgrid_fn = list(stimuli.glob('*.tim'))
+
+def textgrid_filename_to_path(textgrid_filename):
+    f = stimuli / textgrid_filename
+    if not f.exists():
+        raise ValueError(f'{textgrid_filename} does not exist in {stimuli}')
+    return f
+
+def audio_filename_to_path(audio_filename):
+    f = stimuli / audio_filename
+    if not f.exists():
+        raise ValueError(f'{audio_filename} does not exist in {stimuli}')
+    return f
