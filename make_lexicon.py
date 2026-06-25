ca1232a13541b54d90ee88b13ebb28b0731ad993
martijnbentum <bentummartijn@gmail.com>
2026-06-25 13:16:46 +0200

    updated the filter logic

--- make_lexicon.py
+++ make_lexicon.py
@@ -9,6 +9,10 @@ from pathlib import Path
 import random
 from collections import Counter
 
+def save_filtered_word_keys(filtered_word_keys):
+    print('saving filtered word keys')
+    with open('data/filtered_word_keys.pkl', 'wb') as f:
+        pickle.dump(filtered_word_keys, f)
 
 def load_filtered_word_keys():
     print('loading filtered word keys')
@@ -22,12 +26,16 @@ def load_filtered_words():
     words = cgn.load_many(filtered_word_keys)
     return words
 
-def filter_word_tokens_for_lexicon(words = None, min_freq = 100,max_freq = None,
-    min_dur = 100, max_dur = 1500, min_len_label = 2):
+def filter_word_tokens_for_lexicon(words = None, min_freq = 100,
+    max_freq = None, min_dur = 100, max_dur = 1500, filter_out_overlap = True, 
+    filter_out_flemish = True, n_syllables = 1):
     if words is None: words = load_words()
-    words = filter_components(words)
+    if filter_out_overlap: words = remove_overlap(words)
     words = filter_duration(words, min_dur, max_dur)
-    frequency_dict = make_frequency_dict(words, min_len_label = min_len_label)
+    words = filter_components(words)
+    if filter_out_flemish: words = remove_flemish(words)
+    words = filter_on_n_syllables(words, n_syllables)
+    frequency_dict = make_frequency_dict(words, min_len_label = 2)
     words = filter_frequency(frequency_dict, words, min_freq, max_freq)
     return words
 
@@ -49,6 +57,7 @@ def select_words_for_lexicon(filtered_words, n_tokens = 100, random_seed = 0):
     return selection
 
 def load_words():
+    print(f'loading all words from cgn database {cgn}')
     words = list(cgn.words.all())
     return words
 
@@ -60,7 +69,7 @@ def load_syllables():
     syllables = list(cgn.syllables.all())
     return syllables
 
-def filter_components(words = None, remove_components = 'acdhm'):
+def filter_components(words = None, remove_components = 'cdm'):
     if words is None:words = load_words()
     filtered = []
     print(f'filtering components {remove_components} nwords: {len(words)}')
@@ -96,7 +105,46 @@ def filter_frequency(frequency_dict, words = None, min_freq = 100,
         elif max_freq is None and freq >= min_freq: filtered.append(word)
         elif freq >= min_freq and freq <= max_freq: filtered.append(word)
     print(f'filtered {len(words) - len(filtered)} words') 
+    print(f'remaining words: {len(filtered)}')
     return filtered
+
+def remove_overlap(words = None):
+    if words is None: words = load_words()
+    filtered = []
+    print(f'filtering overlapping words nwords: {len(words)}')
+    for word in progressbar(words):
+        if word.overlap: continue
+        filtered.append(word)
+    print(f'filtered {len(words) - len(filtered)} words') 
+    print(f'remaining words: {len(filtered)}')
+    return filtered
+
+def remove_flemish(words = None):
+    if words is None: words = load_words()
+    filtered = []
+    print(f'filtering flemish words nwords: {len(words)}')
+    for word in progressbar(words):
+        dutch_variant = _filename_to_dutch_variant(word.audio.filename)
+        if dutch_variant == 'nl': filtered.append(word)
+        elif dutch_variant != 'vl': 
+            m = f'unknown dutch variant {dutch_variant} for word {word.label}'
+            raise ValueError(m)
+    print(f'filtered {len(words) - len(filtered)} words')
+    print(f'remaining words: {len(filtered)}')
+    return filtered
+        
+
+def filter_on_n_syllables(words = None, n_syllables = 1):
+    if n_syllables is None: return words
+    if words is None: words = load_words()
+    filtered = []
+    print(f'filtering words on n_syllables: {n_syllables} nwords: {len(words)}')
+    for word in progressbar(words):
+        if len(word.syllables) == n_syllables: filtered.append(word)
+    print(f'filtered {len(words) - len(filtered)} words')
+    print(f'remaining words: {len(filtered)}')
+    return filtered
+    
 
 def make_frequency_dict(words = None, min_len_label = 2):
     if words is None:words = load_words()
@@ -112,5 +160,8 @@ def make_frequency_dict(words = None, min_len_label = 2):
         
 def _filename_to_comp(filename):
     return Path(filename).parent.parent.name.split('-')[-1]
+
+def _filename_to_dutch_variant(filename):
+    return Path(filename).parent.name
     
     
