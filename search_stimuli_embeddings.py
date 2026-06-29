9517adbfb1b4d76dc3c21c7df9e128ce0cc0c351
martijnbentum <bentummartijn@gmail.com>
2026-06-29 14:17:54 +0200

    added class and function to compare aligned v misaligned stimuli

--- search_stimuli_embeddings.py
+++ search_stimuli_embeddings.py
@@ -2,10 +2,137 @@ import speech_vector_search as svs
 import stores
 import fiemrok
 import cgn_lexicon_vector_search as clvs
+from progressbar import progressbar
 
 final_wav2vec2_model_name = 'wav2vec2_nl1_checkpoint-200000'
 final_hubert_model_name = 'huibert_nl1_checkpoint-200000'
 name_prefix = 'cgn_center_nucleus'
+
+def make_all_compare_embeddings(layers = [3,6,9,12], experiment = None):
+    if experiment is None: experiment = load_experiment()
+    ces = []
+    output = {}
+    for model_name in [final_wav2vec2_model_name, final_hubert_model_name]:
+        output[model_name] = {}
+        for layer in layers:
+            output[model_name][layer] = {}
+            ce = CompareEmbeddings(layer = layer, model_name = model_name, 
+                experiment = experiment)
+            ce.compute_results()
+            ces.append(ce)
+            output[model_name][layer]['aligned'] = ce.aligned_accuracies
+            output[model_name][layer]['misaligned'] = ce.misaligned_accuracies
+    return output, ces
+    
+
+class CompareEmbeddings:
+    def __init__(self, layer = 6, model_name = final_wav2vec2_model_name,
+        experiment = None, search_vector = None, search_depth = 10):
+        self.layer = layer
+        self.model_name = model_name
+        if experiment is None: experiment = load_experiment()
+        self.experiment = experiment
+        if search_vector is None:
+            search_vector = clvs.load_search_vector_index(layer, name_prefix,
+                model_name = model_name)
+        self.search_vector = search_vector
+        self.search_depth = search_depth
+        self._set_aligned_and_misaligned()
+
+    def __repr__(self):
+        m = f'CompareEmbeddings(layer = {self.layer}, '
+        m += f'model_name = {self.model_name}, '
+        m += f'search_depth = {self.search_depth})'
+        return m
+
+    def _set_aligned_and_misaligned(self):
+        self.aligned = []
+        self.misaligned = []
+        for trial in self.experiment.target_trials:
+            if trial.position != 'Final': continue
+            for stimulus in trial.stimuli:
+                if stimulus.word_type != 'word': continue
+                if stimulus.aligned_type == 'aligned':
+                    self.aligned.append(stimulus)
+                if stimulus.aligned_type == 'misaligned':
+                    self.misaligned.append(stimulus)
+
+    def compute_results(self):
+        self.aligned_results = []
+        self.misaligned_results = []
+        for stimulus in self.aligned:
+            result = self._compute_result_for_stimulus(stimulus)
+            self.aligned_results.append(result)
+        for stimulus in self.misaligned:
+            result = self._compute_result_for_stimulus(stimulus)
+            self.misaligned_results.append(result)
+
+    def _compute_result_for_stimulus(self, stimulus):
+        check_stimulus_ok(stimulus)
+        speech_vector = load_center_frame_nucleus_speech_vector(stimulus, 
+            layer = self.layer, model_name = self.model_name)
+        result = self.search_vector.query(speech_vector, self.search_depth)
+        return result
+
+    @property
+    def aligned_accuracies(self):
+        accuracies = []
+        for stimulus, result in zip(self.aligned, self.aligned_results):
+            accuracy = compute_accuracy(result, stimulus.trial.word)
+            accuracies.append(accuracy)
+        return accuracies
+
+    @property
+    def misaligned_accuracies(self):
+        accuracies = []
+        for stimulus, result in zip(self.misaligned, self.misaligned_results):
+            accuracy = compute_accuracy(result, stimulus.trial.word)
+            accuracies.append(accuracy)
+        return accuracies
+
+    @property
+    def mean_aligned_accuracy(self):
+        accuracies = self.aligned_accuracies
+        return sum(accuracies) / len(accuracies) if accuracies else 0.0
+
+    @property
+    def mean_misaligned_accuracy(self):
+        accuracies = self.misaligned_accuracies
+        return sum(accuracies) / len(accuracies) if accuracies else 0.0
+
+def compute_accuracy(result, label):
+    matches = 0
+    metadatas = result['metadatas']
+    for md, score in zip(metadatas, result['scores']):
+        if md.label == label:
+            matches += 1
+    return matches / len(metadatas) if metadatas else 0.0
+    
+    
+def print_neighbors(label, result, precision=2):
+    """Print `label` (blue) followed by each neighbor's metadata label with
+    its score (light grey) in brackets. Neighbor labels matching `label` are
+    shown in green. Accuracy (fraction of matches) is printed at the end in
+    red between [], e.g.:  loon: lijn (0.72), loon (0.71), ... [0.10]
+    """
+    BLUE = '\033[34m'
+    GREY = '\033[90m'   # bright black = light grey
+    GREEN = '\033[32m'
+    RED = '\033[31m'
+    RESET = '\033[0m'
+
+    metadatas = result['metadatas']
+    parts = []
+    for md, score in zip(metadatas, result['scores']):
+        if md.label == label:
+            name = f'{GREEN}{md.label}{RESET}'
+        else:
+            name = md.label
+        parts.append(f'{name} {GREY}({score:.{precision}f}){RESET}')
+
+    accuracy = compute_accuracy(result, label)
+    print(f'{BLUE}{label}{RESET}: ' + ', '.join(parts)
+          + f' {RED}[{accuracy:.{precision}f}]{RESET}')
 
 def load_experiment():
     stimuli = stores.load_phraser_stimuli_store()
